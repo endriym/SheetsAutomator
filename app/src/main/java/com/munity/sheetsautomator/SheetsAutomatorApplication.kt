2c704905a28fcaa0cfe77fc229493117ee270f1f
endriym <endriymeraj10@hotmail.com>
2025-02-09 03:39:16 +0100

    Instantiate dependencies and inject them manually

--- app/src/main/java/com/munity/sheetsautomator/SheetsAutomatorApplication.kt
+++ app/src/main/java/com/munity/sheetsautomator/SheetsAutomatorApplication.kt
@@ -6,6 +6,8 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import com.munity.sheetsautomator.core.data.local.datastore.SheetsPreferencesDataSource
+import com.munity.sheetsautomator.core.data.remote.SheetsAPIDataSource
+import com.munity.sheetsautomator.core.data.repository.SheetsRepository
 
 private const val DATA_STORE_PREFERENCES_NAME = "preferences"
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
@@ -13,12 +15,21 @@ private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
 )
 
 class SheetsAutomatorApplication : Application() {
-    lateinit var sheetsPreferencesDataSource: SheetsPreferencesDataSource
+    private lateinit var sheetsPreferencesDataSource: SheetsPreferencesDataSource
+    private lateinit var sheetsAPIDataSource: SheetsAPIDataSource
+    lateinit var sheetsRepository: SheetsRepository
 
     override fun onCreate() {
         super.onCreate()
 
         // Manual dependency injection
         sheetsPreferencesDataSource = SheetsPreferencesDataSource(dataStore = this.dataStore)
+        sheetsAPIDataSource = SheetsAPIDataSource()
+        sheetsRepository = SheetsRepository(sheetsPreferencesDataSource, sheetsAPIDataSource)
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        sheetsAPIDataSource.closeClient()
     }
 }
