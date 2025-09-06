a62cfe21f30d3383bfa8a4525c64093307a65006
endriym <endriymeraj10@hotmail.com>
2025-09-06 12:12:47 +0200

    feat: add core models

---
+++ app/src/main/java/com/munity/sheetsautomator/core/data/model/DataEntry.kt
@@ -0,0 +1,8 @@
+package com.munity.sheetsautomator.core.data.model
+
+data class DataEntry(
+    val amount: String,
+    val date: String,
+    val category: String,
+    val description: String,
+)
---
+++ app/src/main/java/com/munity/sheetsautomator/core/data/model/Spreadsheet.kt
@@ -0,0 +1,6 @@
+package com.munity.sheetsautomator.core.data.model
+
+data class Spreadsheet(
+    val id: String,
+    val name: String,
+)
